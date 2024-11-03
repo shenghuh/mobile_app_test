c03b86d61b3dffc46259bcaa6cd7d39efdd11dad
shenghuh <shenghuh@andrew.cmu.edu>
2024-11-03 09:19:10 -0500

    Ionic added

---
+++ mobile-app-test/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mobile_app_test',
+  webDir: 'www'
+};
+
+export default config;
